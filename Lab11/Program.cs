c6c9c8a330e58b99d3e8d76267c3951e28dce8a4
Greateapot <greateapot998@gmail.com>
2023-12-04 21:43:07 +0500

    init

---
+++ Lab11/Program.cs
@@ -0,0 +1,9 @@
+namespace Lab11
+{
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ Lab11Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Lab11Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
